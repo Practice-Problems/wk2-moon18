b74950e476c5be6f4a544595ccbc4291c275b21b
Moonis Rasheed <moonisrasheed18@gmail.com>
2018-09-24 01:00:54 +0500

    Update
    Q1 done Q2 half done, some bugs left to fix

---
+++ Week2.py
@@ -0,0 +1,69 @@
+#Moonis Rasheed
+#Week 2 Programming Practice
+
+import math
+
+#Question-1 Sherlock and Squares
+
+##def squares(a, b):
+##    num_squares=0
+##    if a<=1 or b<=1:
+##        return 0
+##    else:
+##        for i in range(a,b+1):
+##            for j in range((i/2)+1):
+##                if (j*j)==i:
+##                    num_squares=num_squares+1
+##        return num_squares
+def squares(a,b):
+    high_limit=math.floor(math.sqrt(b))
+    lower_limit=math.ceil(math.sqrt(a))
+    return high_limit-(lower_limit+1)
+
+#Question-2 Encryption
+
+def encryption(s):
+    sentence=s.replace(' ', '')
+    sentence_size=len(sentence)
+    lowerbound = math.floor(math.sqrt(sentence_size))
+    upperbound = math.ceil(math.sqrt(sentence_size))
+    total = upperbound * lowerbound
+    sen_list=[]
+    encrypt=""
+    while (total < sentence_size):
+        if (lowerbound < upperbound):
+            lowerbound+=1
+        else: 
+            upperbound+=1
+    
+        total = upperbound * lowerbound
+    for i in range(0,lowerbound):
+        for j in range(0,upperbound):
+            #if (index <= len(s) - 1):
+            sen_list.append([i,j])
+            
+    for i in range(0, upperbound):
+        for j in range(0, lowerbound):
+            if (sen_list[i][j] != 0):
+                encrypt+= sen_list[i][j]
+        
+##    if (col != upperbound - 1):
+##        encrypt+= " "
+    return encrypt
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the encryption function below.
+def e(s):
+    s = input().strip()
+    c = ceil(sqrt(len(s)))
+    return (' '.join(map(lambda x: s[x::c], range(c))))
+
+  
+            
+            
+            
